e944d5419a32669bc14faeb87e87bcff1ed276c5
Escanor Targaryen <57372280+EscanorTargaryen@users.noreply.github.com>
2024-05-24 16:57:12 +0200

    SSR fix error for window reference

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,22 +1,29 @@
 import {afterNextRender, Component} from '@angular/core';
 import {TypographyComponent} from "../../components/typography/typography.component";
 import {TextFont, TextType} from "../../components/typography/TypographyEnums";
-import {CanvaService} from "../../service/canva.service";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-
     TypographyComponent
   ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  // This is a workaround to get the window object in the component, SSR gave error when trying to access window object directly
+  window: any = {
+    innerHeight: 0
+  }
+
   constructor() {
+
+    afterNextRender(() => {
+      this.window = window
+    })
   }
 
   protected readonly TextFont = TextFont;
   protected readonly TextType = TextType;
-  protected readonly window = window;
 }
